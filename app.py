d302cbf0488625637e5ed27ae1674e07b50ac4a4
Nikola325 <nanikolov24a@gmail.com>
2026-01-22 10:41:57 +0200

    Implement basic login system with input checks
    Add a simple login system with input validation.

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
+import streamlit as st
 
+st.title("Login system")
+name = st.text_input("Enter name")
+if st.button("check"):
+if name strip()=="":
+st.warning("Please insert text")
+elif not name isalpha():
+st.warning("-----")
+else st.succes("the text was enter correctly")
