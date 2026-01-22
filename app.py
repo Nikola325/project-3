a37376827876a002f66c04491acfce20a08c91f3
Nikola325 <nanikolov24a@gmail.com>
2026-01-22 10:45:08 +0200

    Fix syntax errors in login validation logic

--- app.py
+++ app.py
@@ -3,8 +3,14 @@ import streamlit as st
 st.title("Login system")
 name = st.text_input("Enter name")
 if st.button("check"):
-if name strip()=="":
-st.warning("Please insert text")
-elif not name isalpha():
-st.warning("-----")
-else st.succes("the text was enter correctly")
+  if name.strip() == "" :
+    st.warning("Please insert text")
+  elif not name.isalpha():
+    st.warning("-----")
+  else:
+    st.succes("the text was enter correctly")
+
+
+
+
+
